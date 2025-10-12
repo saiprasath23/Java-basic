03ab57245a191fdc27b7f18bd76afb2ee82a420c
Sai Prasath k <145656611+saiprasath23@users.noreply.github.com>
2025-10-12 18:34:40 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,20 @@
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		
+		float version = 91.0f;
+		String developer = "Google";
+		boolean beta = false;
+		int relese_year = 2008;
+		char shortcut =  'C';
+
+		System.out.println("Version - " +version);
+		System.out.println("Developer - " +developer);
+		System.out.println("beta - " +beta);
+		System.out.println("Year - "+relese_year);
+		System.out.println("Shortcut - " +shortcut);
+		
+	}
+
+}
